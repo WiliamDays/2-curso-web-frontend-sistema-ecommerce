4b9c4ac4f9fb00fd94fe0ff63df28db37ade53d9
Wilson Nogueira <wilson97nogueira@gmail.com>
2025-05-20 21:44:05 -0300

    add pop-up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-console.log("Hello World");
-
 // Lista de produtos disponíveis com nome e preço
 const produtosDisponiveis = [
   { nome: "Camisa", preco: 50.0 },
@@ -8,5 +6,19 @@ const produtosDisponiveis = [
   { nome: "Boné", preco: 25.0 },
 ];
 
-// Carrinho de compras como um array de objetos
+const produtosEscolhidos = prompt('Qual produto você deseja comprar? (Camisa, Calça, Sapato ou Boné)');
+// Verifica se o produto escolhido está disponível
+console.log(produtosEscolhidos);
+
+let precoProduto = 0;
+
+for (let i = 0; i < produtosDisponiveis.length; i++) {
+  if (produtosEscolhidos === produtosDisponiveis[i].nome) {
+    console.log(`Produto ${produtosEscolhidos} encontrado!`);
+    precoProduto = produtosDisponiveis[i].preco;
+}}
+
+const qtdProduto = prompt (`O produto ${produtosEscolhidos} custa: R$ ${precoProduto.toFixed(2)}. Quantas unidades você deseja adicionar ao carrinho?`);
+console.log(qtdProduto);
 let carrinho = [];
+
