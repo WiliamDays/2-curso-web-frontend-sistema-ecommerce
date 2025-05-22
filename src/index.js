b729bfaee1da41e20c1cc104c54c71a80e01b91a
Wilson Nogueira <wilson97nogueira@gmail.com>
2025-05-21 21:18:23 -0300

    Logica de negocio completa

--- src/index.js
+++ src/index.js
@@ -11,27 +11,40 @@ let carrinho = []
 let maisProdutos
 
 do{
-const produtosEscolhidos = prompt('Qual produto você deseja comprar? (Camisa, Calça, Sapato ou Boné)');
-// Verifica se o produto escolhido está disponível
+  const produtosEscolhidos = prompt('Qual produto você deseja comprar? (Camisa, Calça, Sapato ou Boné)');
+  // Verifica se o produto escolhido está disponível
+
+  for (let i = 0; i < produtosDisponiveis.length; i++) {
+    if (produtosEscolhidos === produtosDisponiveis[i].nome) {
+      console.log(`Produto ${produtosEscolhidos} encontrado!`);
+      precoProduto = produtosDisponiveis[i].preco;
+  }}
+
+  const qtdProduto = prompt (`O produto ${produtosEscolhidos} custa: R$ ${precoProduto.toFixed(2)}. Quantas unidades você deseja adicionar ao carrinho?`);
 
 
+  carrinho.push({
+    nome: produtosEscolhidos,
+    preco: precoProduto,
+    quantidade: qtdProduto,
+  });
 
-for (let i = 0; i < produtosDisponiveis.length; i++) {
-  if (produtosEscolhidos === produtosDisponiveis[i].nome) {
-    console.log(`Produto ${produtosEscolhidos} encontrado!`);
-    precoProduto = produtosDisponiveis[i].preco;
-}}
+  alert(`Você adicionou ${qtdProduto} unidades do produto ${produtosEscolhidos} ao carrinho!`);
 
-const qtdProduto = prompt (`O produto ${produtosEscolhidos} custa: R$ ${precoProduto.toFixed(2)}. Quantas unidades você deseja adicionar ao carrinho?`);
+  maisProdutos = confirm(`Deseja adicionar mais produtos?`);
+  }while(maisProdutos === true)
 
+while (maisProdutos === false) {
 
-carrinho.push({
-  nome: produtosEscolhidos,
-  preco: precoProduto,
-  quantidade: qtdProduto,
-});
+  let valorTotal = 0;
 
-alert(`Você adicionou ${qtdProduto} unidades do produto ${produtosEscolhidos} ao carrinho!`);
+  for (let i = 0; i < carrinho.length; i++) {
+    valorTotal += carrinho[i].preco * carrinho[i].quantidade;
+    alert(`Produtos no carrinho:
+  ${i+1}. ${carrinho[i].nome} - R$ ${carrinho[i].preco.toFixed(2)} x ${carrinho[i].quantidade} = R$ ${(carrinho[i].preco * carrinho[i].quantidade).toFixed(2)}
+  O valor total da compra é: R$ ${valorTotal.toFixed(2)}`);
+  }
+}
 
-maisProdutos = confirm(`Deseja adicionar mais produtos?`);
-}while(maisProdutos === true)
+    
+
